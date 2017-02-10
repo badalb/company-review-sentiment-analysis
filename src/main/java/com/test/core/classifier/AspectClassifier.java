227e1ad30dfa679c572d373f7f2e9513451f8313
Meraj Ashraf <meraj.ashraf@PUMASHRAF.local>
2017-02-10 14:43:29 +0530

    generating text from ods

--- src/main/java/com/test/core/classifier/AspectClassifier.java
+++ src/main/java/com/test/core/classifier/AspectClassifier.java
@@ -1,13 +1,10 @@
 package com.test.core.classifier;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import java.util.*;
+import java.io.FileWriter;
 import org.jopendocument.dom.spreadsheet.Sheet;
-
+import com.test.core.entityextraction.FileReaderWriter;
 import com.test.odsreader.ODSReader;
 
 public class AspectClassifier {
@@ -37,5 +34,31 @@ public class AspectClassifier {
 			List<String> aspectValues = Arrays.asList(aspectValue.split("\\s*,\\s*"));
 			aspectClassDictionary.put(aspectClass, aspectValues);
 		}
+	}
+
+	public static void main(String[] args) {
+		aspectClassDictionary = new HashMap<String, List<String>>();
+//		this.getClass().getClassLoader().
+//		String filePath = "/Users/badalb/AppDirectRabbitMq/glassdoor-review-absa/src/main/resources/aspect-classification-dictionary.ods";
+		String filePath="/Users/meraj.ashraf/Appdirect/project/nlp/new_code/company-review-sentiment-analysis/src/main/resources/appdirect.ods";
+		Sheet sheet = ODSReader.readODS(new File(filePath));
+		List<String> aspectValuesList=new ArrayList<>();
+		int nRowCount = sheet.getRowCount();
+		for (int nRowIndex = 1; nRowIndex < nRowCount; nRowIndex++) {
+			String aspectClass = sheet.getCellAt(0, nRowIndex).getValue().toString().trim();
+			String aspectValue = sheet.getCellAt(1, nRowIndex).getValue().toString().trim();
+			String aspectValueStatus = sheet.getCellAt(1, nRowIndex).getValue().toString().trim();
+			String aspectValuePros=sheet.getCellAt(2, nRowIndex).getValue().toString().trim();
+			String aspectValueCons=sheet.getCellAt(3, nRowIndex).getValue().toString().trim();
+			String aspectValueAdvcMngnt=sheet.getCellAt(4, nRowIndex).getValue().toString().trim();
+			List<String> aspectValues = Arrays.asList(aspectValue.split("\\s*,\\s*"));
+			aspectValuesList.add(aspectValueStatus);
+			aspectValuesList.add(aspectValuePros);
+			aspectValuesList.add(aspectValueCons);
+			aspectValuesList.add(aspectValueAdvcMngnt);
+			aspectClassDictionary.put(aspectClass, aspectValues);
+		}
+		FileReaderWriter.write_file(aspectValuesList, "/Users/meraj.ashraf/Appdirect/project/nlp/new_code/company-review-sentiment-analysis/src/main/resources/output/generated.txt", false);
+//		FileReaderWriter.write_file("", this.allEntityFile, false);
 	}
 }
